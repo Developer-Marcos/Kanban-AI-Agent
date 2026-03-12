857b655df73f7c1fc1c86d1f432505ce05daf759
Marcos <marcosgab2006@gmail.com>
2026-03-11 21:32:05 -0300

    Deploy: Vite Config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  envDir: '../', 
+  envDir: '../',
+  preview: {
+    allowedHosts: [
+      'kortex-frontend-1075947428559.us-east4.run.app'
+    ]
+  }
 })
